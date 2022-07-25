c57b42f8646669520f236338f54ba55f0a3f6182
finefine <tianrandailoving@gmail.com>
2022-07-25 18:23:32 +0800

    update devnet config

--- evm/bench/celt/manager.go
+++ evm/bench/celt/manager.go
@@ -335,11 +335,12 @@ func (m *CeltManager) GetRandomTx(workIndex int, contractIndex int) (*types.Tran
 	rand.Seed(time.Now().UnixNano())
 	random := rand.Intn(101)
 
-	if 1 <= random && random <= 47 {
+	switch {
+	case 1 <= random && random <= 47:
 		return generateGetRewardAndBonusTx(account, contract.NftPool, nonce)
-	} else if 47 < random && random <= 60 {
+	case 47 < random && random <= 60:
 		return generateGetRewardTx(account, contract.NftPool, nonce)
-	} else {
+	default:
 		random := rand.Intn(len(m.worker))
 		return generateTransferTx(account, &(m.worker[random].ethAddress), contract.Celt, nonce)
 	}
