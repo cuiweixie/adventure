46fccd24867093dc6d2be35b7ad5d3c63314aec0
xingqiang.yuan <xingqiang.yuan@okg.com>
2025-05-27 20:52:43 +0800

    opt

--- evm/bench/utils/tpsman.go
+++ evm/bench/utils/tpsman.go
@@ -92,6 +92,7 @@ func (tpsman *SimpleTPSManager) transactionCountAndTimestamp(height uint64) (uin
 }
 
 func (tpsman *SimpleTPSManager) TPSDisplay() {
+	time.Sleep(time.Second * 5)
 	fmt.Println("TPSDisplay")
 	var initHeight uint64
 	var totalTxCount uint64
