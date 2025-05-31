34211eda55c6ece8c30e16311ec1f0d98aa9e2ea
xingqiang.yuan <xingqiang.yuan@okg.com>
2025-06-01 00:36:24 +0800

    update

--- evm/bench/utils/tpsman.go
+++ evm/bench/utils/tpsman.go
@@ -1,11 +1,16 @@
 package utils
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/ethereum/go-ethereum/core/types"
+	"io"
 	"math/big"
+	"net/http"
 	"time"
+
+	"github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -22,6 +27,8 @@ type SimpleTPSManager struct {
 	lastBlockNum uint64
 	maxTPS       float64
 	minTPS       float64
+
+	url string
 }
 
 func NewTPSMan(clientURL string) *SimpleTPSManager {
@@ -39,6 +46,7 @@ func NewTPSMan(clientURL string) *SimpleTPSManager {
 		lastBlockNum:  0,
 		maxTPS:        -1,
 		minTPS:        1000000,
+		url:           clientURL,
 	}
 }
 
@@ -68,52 +76,25 @@ func (tpsman *SimpleTPSManager) BlockHeder(height uint64) *types.Header {
 	}
 }
 
-func (tpsman *SimpleTPSManager) transactionCountAndTimestamp(height uint64) (uint64, uint64) {
-	var header *types.Header
-	var err error
-	for {
-		header, err = tpsman.HeaderByNumber(context.Background(), big.NewInt(int64(height)))
-		if err != nil {
-			time.Sleep(time.Millisecond * 200)
-		} else {
-			break
-		}
-	}
-
-	var txCount uint
-	for {
-		txCount, err = tpsman.TransactionCount(context.Background(), header.ParentHash)
-		if err != nil {
-			time.Sleep(time.Millisecond * 200)
-		} else {
-			return uint64(txCount), header.Time
-		}
-	}
-}
-
 func (tpsman *SimpleTPSManager) TPSDisplay() {
 	time.Sleep(time.Second * 10)
 	fmt.Println("TPSDisplay")
 	var initHeight uint64
 	var totalTxCount uint64
-	var initTime uint64
+	var initTime time.Time
 	for {
 		height := tpsman.GetBlockNum()
-		header, err := tpsman.HeaderByNumber(context.Background(), big.NewInt(int64(height)))
-		if err != nil {
-			panic(err)
-		}
-		txCount, err := tpsman.TransactionCount(context.Background(), header.ParentHash)
+		txCount, err := tpsman.transactionCountByHeight(height)
 		if err != nil {
 			panic(err)
 		}
 		// skip this block
 		if txCount > 0 {
 			initHeight = height
-			initTime = header.Time
+			initTime = time.Now()
 			break
 		} else {
-			fmt.Println("height", height, "parent hash", header.ParentHash, "txcount", txCount)
+			fmt.Println("height", height, "txcount", txCount)
 			time.Sleep(time.Second)
 		}
 	}
@@ -131,23 +112,83 @@ func (tpsman *SimpleTPSManager) TPSDisplay() {
 		}
 
 		for height := lastHeight + 1; height <= newblockNum; height++ {
-			txCount, timestamp := tpsman.transactionCountAndTimestamp(height)
+			txCount, err := tpsman.transactionCountByHeight(height)
+			if err != nil {
+				panic(err)
+			}
 			totalTxCount += txCount
 			lastHeight = height
 
-			avgTPS = float64(totalTxCount) / float64(timestamp-initTime)
+			avgTPS = float64(totalTxCount) / float64(time.Since(initTime).Seconds())
 			if avgTPS > maxTps {
 				maxTps = avgTPS
 			}
 			if avgTPS < minTps {
 				minTps = avgTPS
 			}
-			fmt.Println("========================================================")
-			fmt.Printf("[TPS log] StartBlock Num: %d, NewBlockNum: %d, totalTxCount:%d\n", initHeight+1, height, totalTxCount)
-			fmt.Printf("[Summary] Average BTPS: %5.2f, Max TPS: %5.2f, Min TPS: %5.2f, Time Last: %ds\n", avgTPS, maxTps, minTps, timestamp-initTime)
-			fmt.Println("========================================================")
 		}
+		fmt.Println("========================================================")
+		fmt.Printf("[TPS log] StartBlock Num: %d, NewBlockNum: %d, totalTxCount:%d\n", initHeight+1, lastHeight, totalTxCount)
+		fmt.Printf("[Summary] Average BTPS: %5.2f, Max TPS: %5.2f, Min TPS: %5.2f, Time Last: %ds\n", avgTPS, maxTps, minTps, int64(time.Since(initTime).Seconds()))
+		fmt.Println("========================================================")
 
 		time.Sleep(5 * time.Second)
 	}
+
+}
+
+func (tpsman *SimpleTPSManager) transactionCountByHeight(height uint64) (uint64, error) {
+	// 构造 JSON-RPC 请求
+	requestBody := map[string]interface{}{
+		"jsonrpc": "2.0",
+		"method":  "eth_getBlockByNumber",
+		"params":  []interface{}{fmt.Sprintf("0x%x", height), false}, // false 表示不返回完整交易详情
+		"id":      1,
+	}
+
+	jsonData, err := json.Marshal(requestBody)
+	if err != nil {
+		return 0, fmt.Errorf("error marshaling JSON request: %v", err)
+	}
+
+	// 发送 HTTP 请求
+	resp, err := http.Post(tpsman.url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return 0, fmt.Errorf("error sending HTTP request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// 读取响应
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	// 解析 JSON-RPC 响应
+	var rpcResponse struct {
+		Result *struct {
+			Transactions []string `json:"transactions"` // 当 false 时，这里是交易哈希数组
+		} `json:"result"`
+		Error *struct {
+			Code    int    `json:"code"`
+			Message string `json:"message"`
+		} `json:"error"`
+	}
+
+	if err := json.Unmarshal(body, &rpcResponse); err != nil {
+		return 0, fmt.Errorf("error unmarshaling JSON response: %v", err)
+	}
+
+	// 检查是否有错误
+	if rpcResponse.Error != nil {
+		return 0, fmt.Errorf("JSON-RPC error: %s", rpcResponse.Error.Message)
+	}
+
+	// 检查结果是否为空
+	if rpcResponse.Result == nil {
+		return 0, nil
+	}
+
+	// 返回交易数量
+	return uint64(len(rpcResponse.Result.Transactions)), nil
 }
