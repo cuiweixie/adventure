b3bed473c6fd3553df298b5e07b5c05b18b6bb51
yxq <>
2025-12-11 09:23:19 +0000

    feat: config

--- 9-setup-addr10-combined-30.sh
+++ 9-setup-addr10-combined-30.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 捕获 stdout 和 stderr，并去除 ANSI 颜色代码
-adventure evm bench erc20-init 100000000000000000000000 -i http://127.0.0.1:8123 -a ./config/devnet/acc_pri_10_combined_30 -s 5e3b90aaeec4e3d183044af82167ee799cd3ff5abdd912b9dd89b76e0fd006c4 2>&1 | tee temp_output.txt
+adventure evm bench erc20-init 100000000000000000000000 -i http://127.0.0.1:8123 -a ./config/devnet/acc_pri_10_combined_30 -s 59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d 2>&1 | tee temp_output.txt
 # 从输出中提取合约地址，去除 ANSI 颜色代码
 contract_address=$(cat temp_output.txt | sed 's/\x1b\[[0-9;]*m//g' | grep -o '0x[a-fA-F0-9]\{40\}' | tail -n 1)
 
